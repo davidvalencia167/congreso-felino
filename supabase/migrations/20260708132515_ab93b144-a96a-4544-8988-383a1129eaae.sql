704b632b46f27f501e825893f847c403b953d951
David Valencia <valenciablancodavidsantiago@gmail.com>
2026-08-12 09:38:33 -0500

    supabase and migrations

---
+++ supabase/migrations/20260708132515_ab93b144-a96a-4544-8988-383a1129eaae.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.registrations DROP CONSTRAINT registrations_payment_method_check;
+ALTER TABLE public.registrations ADD CONSTRAINT registrations_payment_method_check CHECK (payment_method IN ('paypal','wompi','bold'));
